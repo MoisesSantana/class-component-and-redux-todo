a4d3667cc391afa743699193244bee0fc4ec74ac
Moisés Santana <moisaant@gmail.com>
2023-07-07 22:36:08 -0300

    :sparkles: handle-task-modal component
    Implementation of the modal for creating and editing a task

---
+++ src/components/handle-task-modal.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Fab } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
+interface HandleTaskModalProps {
+  isNewTask?: boolean;
+}
+
+export class HandleTaskModal extends React.Component<HandleTaskModalProps> {
+  state = {
+    open: false,
+  }
+
+  handleModal = () => {
+   this.setState((prevState: { open: boolean }) => ({ open: !prevState.open }));
+  };
+
+  handleSaveTask = () => {
+    this.handleModal();
+  }
+
+  handleEditTask = () => {
+    this.handleModal();
+  }
+
+  render() {
+    const { open } = this.state;
+    const { isNewTask = false } = this.props;
+    return (
+      <div>
+        <Fab variant="extended" color="primary" aria-label="add" onClick={this.handleModal}>
+          <AddIcon sx={{ mr: 1 }} />
+          New Todo
+        </Fab>
+        <Dialog open={open} onClose={this.handleModal}>
+          <DialogTitle>{ isNewTask ? 'Define your task' : 'Edit your task' }</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="task"
+              label="Task"
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="description"
+              label="Description"
+              fullWidth
+              variant="outlined"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleModal}>Cancel</Button>
+            <Button onClick={isNewTask ? this.handleSaveTask : this.handleEditTask}>Save</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
